60fb3f5df6f90e51d1dfbbed188961a0bce21db3
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-12-07 11:30:55 -0500

    longest sorted string

---
+++ longest.js
@@ -0,0 +1,22 @@
+function longest(s1, s2) {
+  var unique1 = [];
+  var unique2 = [];
+
+  for (i = 0; i < s1.length; i++) {
+    if (!unique1.includes(s1[i]))
+      unique1.push(s1[i]);
+  };
+
+  for (i = 0; i < s2.length; i++) {
+    if (!unique2.includes(s2[i]))
+      unique2.push(s2[i]);
+  };
+
+  final1 = unique1.join('');
+  final2 = unique2.join('');
+
+  if (unique1.length > unique2.length)
+    return unique1;
+  else
+    return unique2;
+}
